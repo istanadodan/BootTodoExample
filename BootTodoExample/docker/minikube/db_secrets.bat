5a99a3f93292b46b61588fabad18592fad74a4bd
michael <dodan.kwon@gmail.com>
2024-09-28 12:14:41 +0900

    test

--- BootTodoExample/docker/minikube/db_secrets.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-kubectl create secret generic db-secrets ^
-  --from-literal=MYSQL_DATABASE=mydb ^
-  --from-literal=MYSQL_PASSWORD=secret ^
-  --from-literal=MYSQL_ROOT_PASSWORD=verysecret ^
-  --from-literal=MYSQL_USER=myuser
--- BootTodoExample/docker/minikube/docker_registry.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-kubectl create secret docker-registry  docker-registry-secret ^
---docker-server=https://index.docker.io/v1/ ^
---docker-username=docker0now ^
---docker-password="asia1394!!" ^
---docker-email=c4now@naver.com ^
--n kaniko
