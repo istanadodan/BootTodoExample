1d11df5ed2fd4cd57642007115d3dbec6724208b
michael <dodan.kwon@gmail.com>
2024-09-28 09:59:56 +0900

    test3

--- BootTodoExample/docker/minikube/init_kube.ps1
+++ BootTodoExample/docker/minikube/init_kube.ps1
@@ -1,10 +1,23 @@
+# 기존 클러스터 삭제
+minikube delete
+
+# 클러스터 생성
+# minikube start --memory 8192 --cpus 4 --static-ip 192.168.49.2 --mount-string "//var/run/docker.sock:/var/run/docker.sock" --mount
+# --mount-string "//var/run/docker.sock:/var/run/docker.sock" `
+# --mount: 마운트 기능을 활성화합니다
+minikube start `
+    --memory 8192 `
+    --cpus 4 `
+    --static-ip 192.168.49.2 `
+    --mount-string="//./pipe/docker_engine://./pipe/docker_engine" `
+    --mount
+
 # docker image
 minikube image load docker-server
 minikube image load docker-mysql
 minikube image load react
 
 $manifestDirectory = @(
-  "D:\works\react-project\BootTodoExample\docker\minikube",
   "D:\works\react-project\reactexample\docker\minikube"
 )
 
@@ -23,9 +36,6 @@ ForEach ($directory in $manifestDirectory) {
 CD $manifestDirectory[0]
 .\db_secrets.bat
 .\registry_auth.bat
-
-# mnt 설정
-CD $manifestDirectory[1]
 .\bg_jobs.ps1
 
 Write-Host "All manifest files from both directories applied successfully."
