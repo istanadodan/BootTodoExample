b19527422d36bdf7a08cbc13290efc4aab89913e
michael <dodan.kwon@gmail.com>
2024-09-28 21:06:13 +0900

    succeed

--- reactexample/docker/minikube/init_kube.ps1
+++ reactexample/docker/minikube/init_kube.ps1
@@ -1,10 +1,22 @@
+minikube delete
+
+minikube start `
+--memory=8192 `
+--cpus=4 `
+--static-ip 192.168.49.2 `
+--docker-opt 'group=1001'
+# --mount-string= "/var/run/docker.sock:/var/run/docker.sock" `
+# --mount `
+# --host-only-cidr=192.168.55.1/24 `
+# --network minikube `
+
 # docker image
-minikube image load docker-server
+# minikube image load docker-server
 minikube image load docker-mysql
 minikube image load react
+minikube image load docker-jenkins
 
 $manifestDirectory = @(
- # "D:\works\react-project\BootTodoExample\docker\minikube",
   "D:\works\react-project\reactexample\docker\minikube"
 )
 
@@ -19,13 +31,11 @@ ForEach ($directory in $manifestDirectory) {
   }
 }
 
+cd $manifestDirectory[0]
 # db 설정
-CD $manifestDirectory[0]
 .\db_secrets.bat
 .\registry_auth.bat
-
 # mnt 설정
-CD $manifestDirectory[1]
 .\bg_jobs.ps1
 
 Write-Host "All manifest files from both directories applied successfully."
