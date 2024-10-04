b47b9b66fd832320223f307ab4628bfa7a5490b8
michael <dodan.kwon@gmail.com>
2024-10-04 16:27:53 +0900

    test

--- Frontend/src/pages/task-details/index.tsx
+++ Frontend/src/pages/task-details/index.tsx
@@ -6,7 +6,7 @@ import { ToggleTask } from "features/toggle-task";
 import { TaskCard, taskModel } from "entities/task";
 
 import styles from "./styles.module.scss";
-// import AA from "entities/board/ui/ApiComponentList"
+import AA from "entities/board/ui/ApiComponent"
 
 const TaskDetails = () => {
   const dispatch = useDispatch();
@@ -44,7 +44,7 @@ const TaskDetails = () => {
           actions={[<ToggleTask key="toggle" taskId={+taskId!} />]}
         />
       </Layout.Content>
-      {/* <AA /> */}
+      <AA />
     </Layout>
   );
 };
