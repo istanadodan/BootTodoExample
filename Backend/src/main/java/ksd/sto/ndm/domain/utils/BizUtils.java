8458ed05b830b2480f8484dd6b462e290e875fd6
michael <dodan.kwon@gmail.com>
2025-03-10 23:01:12 +0900

    fix commit
    	modified:   build.gradle
    	modified:   src/main/java/ksd/sto/ndm/domain/controller/GrafanaApiController.java
    	modified:   src/main/java/ksd/sto/ndm/domain/utils/BizUtils.java
    	modified:   bin/main/application.yml
    	deleted:    hs_err_pid33320.log
    	modified:   ../docker/minikube/grafana-deployment.yaml

--- Backend/src/main/java/ksd/sto/ndm/domain/utils/BizUtils.java
+++ Backend/src/main/java/ksd/sto/ndm/domain/utils/BizUtils.java
@@ -1,33 +1,56 @@
 package ksd.sto.ndm.domain.utils;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.security.Key;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
 import java.util.Date;
+
+import org.bouncycastle.openssl.PEMKeyPair;
+import org.bouncycastle.openssl.PEMParser;
+
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
+@Slf4j
 public class BizUtils {
-    public static String generateJwt(String privateKeyPath, String issuer, String subject,
-            int expirationMinutes) throws Exception {
-        // 개인 키 파일 읽기 byte[]
-        String privateKeyContent = Files
-            .readString(Paths.get(privateKeyPath))
-            .replace("-----BEGIN RSA PRIVATE KEY-----", "")
-            .replace("-----END RSA PRIVATE KEY-----", "")
-            .replaceAll("\\s", "");
+    public static String generateJwt(String privateKeyPath, String user, String email,
+            int expirationMinutes) {
 
-        Key key = Keys.hmacShaKeyFor(privateKeyContent.getBytes(StandardCharsets.UTF_8));
+        PrivateKey publicKey = null;
+        try (Reader reader = new FileReader(privateKeyPath);
+                PEMParser pemParser = new PEMParser(reader)) {
 
-        // JWT 생성
-        Date now = new Date();
-        Date expiration = new Date(now.getTime() + (expirationMinutes * 60 * 1000));
-        return Jwts
-            .builder()
-            .subject(subject)
-            .issuer(issuer)
-            .issuedAt(now)
-            .expiration(expiration)
-            .signWith(key)
-            .compact();
+            Object obj = pemParser.readObject();
+            byte[] decodedKey = ((PEMKeyPair) obj).getPrivateKeyInfo().getEncoded();
+            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+
+            // PKCS8 형식의 개인 키 스펙 생성
+            publicKey = keyFactory.generatePrivate(new PKCS8EncodedKeySpec(decodedKey));
+
+            // JWT 생성
+            Date now = new Date();
+            Date expiration = new Date(now.getTime() + (expirationMinutes * 60 * 1000));
+            return Jwts
+                .builder()
+                .subject(email)
+                .claim("user", user)
+                .issuedAt(now)
+                .expiration(expiration)
+                .signWith(publicKey)
+                .compact();
+        } catch (FileNotFoundException e) {
+            log.error("private-key를 불러올 수 없습니다");
+        } catch (IOException e) {
+        } catch (NoSuchAlgorithmException e) {
+            log.error("RSA 키생성 알고리즘이 존재하지 않습니다");
+        } catch (InvalidKeySpecException e) {
+            log.error("개인키 생성에 실패했습니다");
+        }
+        return null;
     }
 }
